f4bd1fdabb234c5e0fa3a3186193c9a346fbfdbd
Eugene Babeshko <63898296+ebabeshko@users.noreply.github.com>
2024-05-17 18:41:25 +0300

    Add hide-inspect-panel-json.bat

---
+++ hands-on/hide-inspect-panel-json.bat
@@ -0,0 +1,2 @@
+rem Hide Inspect Panel JSON for Viewers (Grafana 10.4)
+fart -r -c -- public/build/*.js "n.meta.canEdit&&U.push({text:(0,f.t)(\"panel.header-menu.query\",\"Query\"),onClick:u=>d(F.q.Query)})),U.push({text:(0,f.t)(\"panel.header-menu.inspect-json\",\"Panel JSON\")" "n.meta.canEdit&&U.push({text:(0,f.t)(\"panel.header-menu.query\",\"Query\"),onClick:u=>d(F.q.Query)})),n.meta.canEdit&&U.push({text:(0,f.t)(\"panel.header-menu.inspect-json\",\"Panel JSON\")"
